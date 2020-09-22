bd1c000e52f70bef3a37f741093d78408be4bec7
Vibhanshu <VibhanshuV00@gmail.com>
2020-09-22 13:45:03 +0530

    added FAQ Questions

--- lib/Pages/Student/HomePageSources/FAQ.dart
+++ lib/Pages/Student/HomePageSources/FAQ.dart
@@ -6,16 +6,39 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-final questions = ['Q1. Which degree is superior, B.E. or B. Tech. in terms of job scope and future?',
-  'Q2. If two colleges hold equal status, then what should I prefer, B.E. or B. Tech?',
-  'Q3. What is the difference between science and engineering?',
-  'Q4. Is it possible to pursue regular B.Tech and diploma course at one time?',
-  'Q5. Can JoSAA prepare / announce JEE Main 2020 ranks?'];
-final answer = ['Ans. Both degrees have equal recognition with equal scope and future. More precisely, it’s your college and branch that matters.',
-  'Ans. If a student is interested in research-oriented work and wants to pursue higher studies then B.E. may be preferred over B. Tech. However, in the present situation analysis shows that course content is almost the same but the approach may be different.',
-  'Ans. Science is knowledge based on observed facts and tested truths arranged in an orderly system that can be validated and communicated to other people. Engineering is the creative application of scientific principles used to plan, build, direct, guide, manage, or work on systems to maintain and improve our daily lives.',
-  'Ans. Two regular courses can\'t be pursued together. One course should be regular and the other should be distance or short-term certificate course.',
-  'Ans. No. JEE Main 2020 ranks are provided by the JEE Apex Board. JoSAA 2020 uses these ranks as such without any further change.'];
+final questions = ['Q1. How do I get an idea of the seat I am likely to be allotted on the basis of rank secured by me??',
+  'Q2. If a candidate has mistakenly opted for Deemed University’s seats instead of AIQ does he/she have the option to correct it?',
+  'Q3. Am I eligible to participate in the subsequent rounds of counselling if I have joined the seat allotted to me in round one of NEET counselling 2020?',
+  'Q4. Do those candidates who do not join the college allotted to them in round 2 of counselling of NEET and wish to participate in the mop-up round for Deemed/Central universities need to register afresh?',
+  'Q5. Which candidates are not eligible for the second round of NEET counselling 2020?',
+  'Q6. After joining the college allotted to me in the second round of AIQ seats counselling can I opt to join state/private quota seats?',
+  'Q7. Is fresh registration required for round 2 of counselling of NEET if I have already registered for round 1?',
+  'Q8. Will the authorities enable fresh registrations for stray vacancy round?',
+  'Q9. How can I retrieve the password in case I forget it?',
+  'Q10. How many choices can the candidate give for college and course?',
+  'Q11. What if I do not enter any choice during the choice filling process?',
+  'Q12. When does the authority forfeit the security money?',
+  'Q13. How is the refund of the security amount of the counselling for NEET initiated?',
+  'Q14. Can upgradation facility be availed in the second round of counselling for Deemed/ Central Universities to Mop-up Round counseling of DGHS for Central / Deemed Universities?',
+  'Q15. If my upgraded choice is allotted to me after round 1 do I have to join the allotted college?',
+  'Q16. What will happen if I do not join the college allotted to me after upgradation?'];
+
+final answer = ['Ans. For this candidates can refer to previous year seat allotment lists to get an indicative idea of the seat they might get allotted on the basis of their rank. However it must be noted that there is no guarantee of the same and no certainty of the data provided. Another way of knowing this is the Mock counselling and Indicative seat allotment done during the registration period of round one of counselling of NEET',
+  'Ans. Yes, candidates have the one-time opportunity to correct this mistake by by using the reset initialization option which is provided at the time of NEET registration 2020',
+  'Ans. Those candidates who have been allotted a college in the first round will have to join to complete the admission formalities following which they can choose to participate in the subsequent rounds and also opt for upgradation. Candidates have been provided with the facility of free entry and exit in the round 1 of NEET 2020 counselling so they can surrender the allotted seats and participate in round 2. For the aforementioned round candidates can use their login credentials of round 1. Those of them who do not join the allotted college in round 1 can also participate in round 2.',
+  'Ans. Of course yes, such candidates will have to register themselves afresh for the mop-up round of counselling conducted by Deemed/Central universities. However, such candidates will have their security amount forfeited.',
+  'Ans. Those candidates who have furnished incomplete or inadequate documents for the admission process of NEET 2020 will not be eligible to participate in the second round.',
+  'Ans. It is a guideline by the Honorable Supreme Court that once allotted a seat in the second round of counselling, a candidate cannot vacate it. In case a candidate has been allotted a seat in round 2, they can exit by getting their security money forfeited.',
+  'Ans. No, already registered candidates are not required to re-register themselves for round 2, they can use the login credentials of round 1.,
+  'Ans. No, DGHS will send a list of candidates ten times the vacant seats and the Deemed/Central universities will conduct the final stray vacancy round.',
+  'Ans. In order to retrieve the password candidates will have to re-enter the details filled in by them at the time of registration. This includes the answer to the security question. Candidates will then be able to set a new password for future logging purposes, provided the data entered by them matches the previously entered information.',
+  'Ans. Candidates can opt for as many colleges and courses as many they want at the time of choice filling. The authorities have not put any restrictions or limits on it.',
+  'Ans. In case a candidate does not enter any choice then he/she will not be allotted any college in the counselling process.',
+  'Ans. In the below mentioned cases security money of candidates will be forfeited:\n•	In case the candidate is allotted a seat in the second or mop-up round and he/she does not wish to join the allotted college\n•	In a situation where the candidate furnishes incorrect or inadequate information at any step of the admission process',
+  'Ans. The authorities will refund the security money at the end of the counselling process. If the candidate has been allotted a seat in NEET counselling 2020 then his/her security amount is refunded to the respective institution and is adjusted with the annual fee. In case the candidate is not allotted any seat throughout the counselling process, his/her security deposit will be refunded to the bank account, details of which were given at the time of registration.',
+  'Ans. Candidates who join the college allotted to them in the second round of counselling for Deemed/Central universities will not have the upgradation facility. Also, such candidates cannot vacate the AIQ seats. Also, these candidates will no longer be eligible to participate in the further rounds of counselling including the stray round.',
+  'Ans. Yes, because in such a situation the seat allotted to you in round 1 will automatically get cancelled. After seat allotment result is published candidates will have to report to the college alloted in round 2 with a relieving letter from the college allotted in round 1 of counselling of NEET.',
+  'Ans. If you do not take admission to the seat allotted to you after upgradation then your security money will be forfeited and thereafter you can register yourself afresh for the mop-up round of counselling.'];
 final expanded=[false,false,false,false,false];
 class FAQ extends StatefulWidget{
   static _FAQ of(BuildContext context) => context.ancestorStateOfType(const TypeMatcher<_FAQ>());
